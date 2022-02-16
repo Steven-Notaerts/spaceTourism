9c5f6f5088ca7a6f37c0cfc8f62b175aefe50fd8
steven-notaerts <notaerts.steven@gmail.com>
2022-02-16 10:40:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spaceTourism
+ 
---
+++ javaScript/header.js
@@ -0,0 +1,33 @@
+﻿const primaryNav = document.querySelector(".navigation");
+const navToggle = document.querySelector(".header__toggle-menu-btn");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+const activeLink = document.getElementsByClassName("navigation__link");
+console.log(activeLink);
+
+// for (let i = 0; i < activeLink.length; i++) {
+//   addEventListener("click", function (event) {
+//     event.preventDefault();
+//     if (activeLink) {
+//       console.log("ok" + activeLink[i].textContent);
+//     }
+//   });
+// }
+
+const addActiveClass = () => {
+  for (let i = 0; i < activeLink.length; i++) {
+    console.log(i);
+  }
+};
+activeLink.addEventListener("click", addActiveClass());
