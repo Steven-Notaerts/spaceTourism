2745c60274691ee54da3ec807772ec0e9c76811e
Steven Notaerts <notaerts.steven@gmail.com>
2022-03-09 18:51:11 +0100

    update

--- javaScript/crew.js
+++ javaScript/crew.js
@@ -5,10 +5,11 @@ const {crew} = require('../utils/data.json');
 console.log(crew);
 
 const crewSubNav = document.querySelectorAll(".crew-sub-nav__item");
-const crewMemberPicture = document.querySelectorAll(".crew__member-image");
-const crewFunction = document.querySelectorAll(".crew__function")[0];
-const crewName = document.querySelectorAll(".crew__name")[0];
-const crewMemberInformation = document.querySelectorAll(".crew__member-information")[0];
+let crewMemberPicture = document.querySelectorAll(".crew__member-image")[0];
+console.log(crewMemberPicture.src)
+let crewFunction = document.querySelectorAll(".crew__function")[0];
+let crewName = document.querySelectorAll(".crew__name")[0];
+let crewMemberInformation = document.querySelectorAll(".crew__member-information")[0];
 console.log(crewFunction)
 console.log(crewName);
 console.log(crewMemberInformation)
@@ -24,32 +25,35 @@ crewSubNav.forEach((crewButton) => {
        })
 
        //Douglas Hurley
-       if(crewButton.classList.contains("douglas")){    
-      crewFunction.innerHTML = `${crew[0].role}`;
-        crewName.innerHTML = `${crew[0].name}`;
-        crewMemberInformation.innerHTML= `${crew[0].bio}`;        
-       // mainDestinationsImage.src = `${destinations[0].images.webp}`;           
+       if(crewButton.classList.contains("douglas")){  
+         //  crewMemberPicture.setAttribute('src', '')  
+         crewFunction.innerHTML = `${crew[0].role}`;
+         crewName.innerHTML = `${crew[0].name}`;
+         crewMemberInformation.innerHTML= `${crew[0].bio}`;        
+        // crewMemberPicture.src = `${crew[0].images.webp}`;   
+         crewMemberPicture.setAttribute('src', `${crew[0].images.webp}`)          
      } 
     //MARK SHUTTLEWORTH
      if(crewButton.classList.contains("mark")){    
-      crewFunction.innerHTML = `${crew[1].role}`;
-        crewName.innerHTML = `${crew[1].name}`;
-        crewMemberInformation.innerHTML= `${crew[1].bio}`;        
-       // mainDestinationsImage.src = `${destinations[0].images.webp}`;           
+         crewFunction.innerHTML = `${crew[1].role}`;
+          crewName.innerHTML = `${crew[1].name}`;
+          crewMemberInformation.innerHTML= `${crew[1].bio}`;        
+          crewMemberPicture.src = "../assets/image/crew/image-douglas-hurley.png";  
+         // mainDestinationsImage.src = `${destinations[0].images.webp}`;           
      }
      //Victor Glover
     if(crewButton.classList.contains("victor")){    
-      crewFunction.innerHTML = `${crew[2].role}`;
-        crewName.innerHTML = `${crew[2].name}`;
-        crewMemberInformation.innerHTML= `${crew[2].bio}`;        
-       // mainDestinationsImage.src = `${destinations[0].images.webp}`;           
+          crewFunction.innerHTML = `${crew[2].role}`;
+           crewName.innerHTML = `${crew[2].name}`;
+           crewMemberInformation.innerHTML= `${crew[2].bio}`;        
+        crewMemberPicture.setAttribute('src', `${crew[2].images.webp}`)                  
      }
      //Anousheh Ansari
      if(crewButton.classList.contains("anousheh")){    
-      crewFunction.innerHTML = `${crew[3].role}`;
-        crewName.innerHTML = `${crew[3].name}`;
-        crewMemberInformation.innerHTML= `${crew[3].bio}`;        
-       // mainDestinationsImage.src = `${destinations[0].images.webp}`;           
+         crewFunction.innerHTML = `${crew[3].role}`;
+         crewName.innerHTML = `${crew[3].name}`;
+         crewMemberInformation.innerHTML= `${crew[3].bio}`;        
+        crewMemberPicture.src = `${crew[3].images.webp}`;                 
      }
 
     })
