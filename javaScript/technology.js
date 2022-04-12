1d4c81b3ed5ac5b959a466a1ed37e89f07a3aa5f
Steven Notaerts <notaerts.steven@gmail.com>
2022-04-12 17:20:11 +0200

    Update technology.js
    fix image change on click submenu

--- javaScript/technology.js
+++ javaScript/technology.js
@@ -1,44 +1,32 @@
 const {technology} = require('../utils/data.json');
-console.log(technology)
-const technologySubNav = document.querySelectorAll(".technology__list-item");
-const technologyImage = document.querySelectorAll(".technology__tech-image");
-const technologyTitle = document.querySelectorAll(".technology__tech-title")[0];
-const technologyDescription = document.querySelectorAll(".technology__tech-description")[0];
+let technologySubNav = document.querySelectorAll(".technology__list-item");
+let technologyImage = document.querySelectorAll(".technology__tech-image")[0]
+let technologyTitle = document.querySelectorAll(".technology__tech-title")[0];
+let technologyDescription = document.querySelectorAll(".technology__tech-description")[0];
 
 technologySubNav.forEach((technologyButton) => {
-    console.log(technologyButton)
-
-    technologyButton.addEventListener("click", function(){
-        
+    technologyButton.addEventListener("click", function(){        
         technologySubNav.forEach((technologyubButton) => {
             technologyubButton.classList.remove('technology-sub-nav-active');
             this.classList.add('technology-sub-nav-active')
         })
 
         if(technologyButton.classList.contains('launch-vehicle')){
-            console.log("launch")
-            console.log(technologyTitle)
-            console.log(`${technology[0].name}`)
-                technologyTitle.innertext = `${technology[0].name}`;
-                technologyDescription.innertext = `${technology[0].description}`;
+                technologyTitle.innerText = `${technology[0].name}`;
+                technologyDescription.innerText = `${technology[0].description}`;
+                technologyImage.setAttribute('src', `${technology[0].images.landscape}`)     
             }
 
-             if(technologyButton.classList.contains('spaceport')){
-                  console.log(`${technology[1].name}`)
-                 console.log("spaceport")
-                  console.log(technologyTitle)
-                technologyTitle.innertext = `${technology[1].name}`;
-                technologyDescription.innertext = `${technology[1].description}`;
+        if(technologyButton.classList.contains('spaceport')){                 
+                technologyTitle.innerText = `${technology[1].name}`;
+                technologyDescription.innerText = `${technology[1].description}`;
+                 technologyImage.setAttribute('src', `${technology[1].images.landscape}`)   
             }
 
-             if(technologyButton.classList.contains('space-capsule')){
-                  console.log(`${technology[2].name}`)
-                 console.log("space-capsule")
-                  console.log(technologyTitle)
-               technologyTitle.innertext = `${technology[2].name}`;
-                technologyDescription.innertext = `${technology[2].description}`;
+        if(technologyButton.classList.contains('space-capsule')){
+               technologyTitle.innerText = `${technology[2].name}`;
+               technologyDescription.innerText = `${technology[2].description}`;
+                technologyImage.setAttribute('src', `${technology[2].images.landscape}`)   
             }
-
-
     })
 })
